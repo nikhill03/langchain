e75877f60c252a8dcc4ce04ae3fab8e98306b5c8
nikhill03 <nikhilldev03@gmail.com>
2026-03-08 14:12:00 +0530

    Research Tool Assistant updated

--- ResearchToolAssistant.py
+++ ResearchToolAssistant.py
@@ -18,7 +18,12 @@ model = ChatHuggingFace(llm=llm)
 
 st.header('Research Tool')
 
+# Dynamic Inputs
+paper_input = st.selectbox( "Select Research Paper Name", ["Select...", "Attention Is All You Need","BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"])
 
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code- Oriented", "Mathematical"] )
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
 
 # template
