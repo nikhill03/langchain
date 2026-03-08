03ca71ea4689ba167d9f1311c6c91067e00ce997
nikhill03 <nikhilldev03@gmail.com>
2026-03-08 14:11:26 +0530

    Research Tool Assistant with dynamic prompt

---
+++ ResearchToolAssistant.py
@@ -0,0 +1,53 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import streamlit as st
+import os
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+hf_token = os.getenv("HUGGINGFACE_ACCESS_TOKEN")
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Meta-Llama-3-8B-Instruct",
+    huggingfacehub_api_token=hf_token,
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+st.header('Research Tool')
+
+
+
+
+# template
+template = PromptTemplate(
+    template="""
+        Please summarize the research paper titled "{paper_input}" with the following
+        specifications:
+        Explanation Style: {style_input}
+        Explanation Length: {length_input}
+        1. Mathematical Details:
+        - Include relevant mathematical equations if present in the paper.
+        - Explain the mathematical concepts using simple, intuitive code snippets
+        where applicable.
+        2. Analogies:
+        - Use relatable analogies to simplify complex ideas.
+        If certain information is not available in the paper, respond with: "Insufficient
+        information available" instead of guessing.
+        Ensure the summary is clear, accurate, and aligned with the provided style and
+        length.
+    """,
+    input_variables=['paper_input','style_input','length_input']
+)
+
+prompt = template.invoke({
+            'paper_input': paper_input,
+            'style_input': style_input,
+            'length_input': length_input
+        })
+
+if st.button('Summarize'):
+    result = model.invoke(prompt)
+    st.text(result.content)
