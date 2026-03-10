cad9ce477e60b05bc511f88b21d8bad03d97cc98
nikhill03 <nikhilldev03@gmail.com>
2026-03-10 11:22:50 +0530

    Improved chatbot with basic chat history implementation

--- chatbot.py
+++ chatbot.py
@@ -14,9 +14,17 @@ llm = HuggingFaceEndpoint(
 
 model = ChatHuggingFace(llm = llm)
 
+# for context/ memory of the chat
+chat_history = []
+
+
 while True:
     user_input = input('You : ')
+    chat_history.append(user_input)
     if user_input == 'exit':
         break
     result = model.invoke(user_input)
+    chat_history.append(result.content)
     print("AI : ",result.content)
+
+print(chat_history)
