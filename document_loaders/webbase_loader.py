345bcc964a0ec0197b4d1808f518dd0426c278ec
nikhill03 <nikhilldev03@gmail.com>
2026-03-27 13:53:41 +0530

    Webbase loader

---
+++ document_loaders/webbase_loader.py
@@ -0,0 +1,35 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+hf_token = os.getenv("HUGGINGFACE_ACCESS_TOKEN")
+
+llm = HuggingFaceEndpoint(
+    repo_id= "meta-llama/Meta-Llama-3-8B-Instruct",
+    huggingfacehub_api_token= hf_token,
+    task = "text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+prompt = PromptTemplate(
+    template='Answer the following question \n {question} from the following text - \n {text}',
+    input_variables=['question','text']
+)
+
+parser = StrOutputParser()
+
+url = 'https://www.flipkart.com/apple-macbook-air-m2-16-gb-256-gb-ssd-macos-sequoia-mc7x4hn-a/p/itmdc5308fa78421'
+loader = WebBaseLoader(url)
+
+docs = loader.load()
+
+
+chain = prompt | model | parser
+
+print(chain.invoke({'question':'What is the product that we are talking about?', 'text':docs[0].page_content}))
